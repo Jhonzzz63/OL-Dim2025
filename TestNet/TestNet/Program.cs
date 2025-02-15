1c43127b0b15b96ca002ad67bf733f10e8b3b144
jhonzzz63 <jhonzzz63@gmail.com>
2025-02-15 10:23:59 -0600

    First Commit

---
+++ TestNet/TestNet/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Prpgram
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Introduce your name : ");
+        string Name = Console.ReadLine();
+
+        Console.WriteLine($"Hola : {Name}");  
+    }
+}
